8c57c519fb5f64462f5e477d60ad824b897dce32
Aliaksandr Dvornik <aadvornik@gmail.com>
2021-10-02 00:30:26 +0300

    Updated

--- main.py
+++ main.py
@@ -4,9 +4,19 @@ by Aliaksandr Dvornik
 '''
 
 #read spectrum file
-inf = open('spec.spe', 'r')
-l = [line.strip() for line in inf]
-del l[:7]
+try:
+    inf = open('spec.spe', 'r')
+    l = [line.strip() for line in inf]
+    del l[:7]
 #print(type(l))
-for i in l:
-    print(i)
+    for i in l:
+        date_mea = l[1]
+        cps = float(l[5])
+        #print(i)
+    del l[:15]
+    for i in l:
+        print(i)
+    #print(date_mea)
+    #print(cps)
+except IOError:
+    print("No file")
