905021d181ce30807ad279d88b132169d5895492
Aliaksandr Dvornik <aadvornik@gmail.com>
2021-10-02 00:00:16 +0300

    Updated

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ by Aliaksandr Dvornik
 '''
 
 #read spectrum file
-with open('spec.spe', 'r') as inf:
-    for line in inf:
-        print(line)
+inf = open('spec.spe', 'r')
+l = [line.strip() for line in inf]
+del l[:7]
+#print(type(l))
+for i in l:
+    print(i)
