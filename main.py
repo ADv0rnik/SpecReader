8e41364fb59fd0dca2fe61bc733de414a10d5bee
Aliaksandr Dvornik <aadvornik@gmail.com>
2021-10-20 00:18:44 +0300

    time variable added

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ try:
 
     for i in l:
         date_mea = l[1]
+        time = int(l[3].split()[0])
         cps = float(l[5])
         #print(i)
     del l[:15]
@@ -20,12 +21,13 @@ try:
     counts = [l[i] for i in range(1, num_of_chanels+2)]
     for a in range(1, num_of_chanels):
         counts_in_chanel[a] = counts[a]
-    # for key, value in d.items():
+    # for key, value in counts_in_chanel.items():
     #     print(key, value)
-    for i in l:
-        print(i)
-    #print(date_mea)
-    #print(cps)
-    #print(num_of_chanels)
+    # for i in l:
+    #     print(i)
+    # print(time)
+    # print(date_mea)
+    # print(cps)
+    # print(num_of_chanels)
 except IOError:
     print("No file")
