9243085fd7acf6ed847b5836ffb5a3e9b98761cf
Aliaksandr Dvornik <aadvornik@gmail.com>
2021-11-07 23:43:55 +0300

    Class was imported into the main.py. "Parser" is a built-in library. The name was changed to "data_pars"

--- main.py
+++ main.py
@@ -1,40 +1,13 @@
-'''
-version 0.9.1
-by Aliaksandr Dvornik
-'''
+"""
+executive file
+to be continued...
+"""
+from spec_pars import Parser
 
-#read spectrum file
-counts = []
-energy_list=[]
-counts_in_chanel={}
-try:
-    inf = open('spec.spe', 'r')
-    l = [line.strip() for line in inf]
-    del l[:7]
+if __name__ == '__main__':
+    fname = 'spec.spe'
+    pars = Parser(fname)
+    line=pars.get_lines()
 
-    for i in l:
-        date_mea = l[1]
-        time = int(l[3].split()[0])
-        cps = float(l[5])
-        #print(i)
-    del l[:15]
-    num_of_chanels = int(l[0].split()[1])+1 # reading number of chanels
-    counts = [l[i] for i in range(1, num_of_chanels+2)] # reading counts in each chanel
-    for a in range(1, num_of_chanels):
-        counts_in_chanel[a] = counts[a]
-    del l[:1031]
-    en = [l[i].split() for i in range(0, num_of_chanels)]
-    for elem in en:
-        energy_list.append(elem[1])
-    # for key, value in counts_in_chanel.items():
-    #     print(key, value)
-
-    for i in range(len(energy_list)):
-        print(energy_list[i], end ='\n')
-
-    # print(time)
-    # print(date_mea)
-    # print(cps)
-    # print(num_of_chanels)
-except IOError:
-    print("No file")
+    for i in range(len(line)):
+        print(line[i])
