8b64c0e9caac8e02aff1a9d6da4c46bb64d31559
Aliaksandr Dvornik <aadvornik@yandex.ru>
2021-11-08 14:24:00 +0300

    Updated

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 """
-executive file
-to be continued...
+version 0.9.1
+by Aliaksandr Dvornik
 """
+
 from data.spec_pars import Parser
 
 if __name__ == '__main__':
     fname = 'spec.spe'
     pars = Parser(fname)
-    line=pars.get_lines()
+    line = pars.get_lines()
+    pars.remove_lines(line, 7)
+    # get data from spectrum file
+    date_mea = pars.get_param(line)[0] #date of measurements
+    time = pars.get_param(line)[1] #time of measurements
+    cps = pars.get_param(line)[2] #total counts per second
+
+
 
     for i in range(len(line)):
         print(line[i])
