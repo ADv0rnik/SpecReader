6e404516e88d1cdd1fc688c48e87dbfd8a164d6e
Aliaksandr Dvornik <aadvornik@yandex.ru>
2021-11-08 14:28:11 +0300

    Updated

--- main.py
+++ main.py
@@ -5,16 +5,19 @@ by Aliaksandr Dvornik
 
 from data.spec_pars import Parser
 
+num_of_chanels = 1023
+
 if __name__ == '__main__':
     fname = 'spec.spe'
     pars = Parser(fname)
+    # read spectrum file
     line = pars.get_lines()
     pars.remove_lines(line, 7)
     # get data from spectrum file
     date_mea = pars.get_param(line)[0] #date of measurements
     time = pars.get_param(line)[1] #time of measurements
     cps = pars.get_param(line)[2] #total counts per second
-
+    pars.remove_lines(line, 16)
 
 
     for i in range(len(line)):
