1354d898322279c502cb5200a9dc45cfd2318dbb
Aliaksandr Dvornik <aadvornik@gmail.com>
2021-11-08 20:40:08 +0300

    Dataframe created

--- data/df_constructor.py
+++ data/df_constructor.py
@@ -1,36 +1,26 @@
 import pandas as pd
-import numpy as np
-
 from data.spec_pars import Parser
 
 num_of_chanels = 1023
 
-if __name__ == '__main__':
-    fname = 'spec.spe'
-    pars = Parser(fname)
-    # read spectrum file
-    line = pars.get_lines()
-    pars.remove_lines(line, 7)
-    # get data from spectrum file
-    date_mea = pars.get_param(line)[0] #date of measurements
-    time = pars.get_param(line)[1] #time of measurements
-    cps = pars.get_param(line)[2] #total counts per second
-    pars.remove_lines(line, 16)
-    #get list of counts per chanel
-    counts = pars.get_counts(line, num_of_chanels)
-    pars.remove_lines(line, 1030)
-    #get list of energy
-    energy = pars.get_energy_list(line, num_of_chanels)
+fname = 'spec.spe'
+pars = Parser(fname)
+# read spectrum file
+line = pars.get_lines()
+pars.remove_lines(line, 7)
+# get data from spectrum file
+date_mea = pars.get_param(line)[0]  # date of measurements
+time = pars.get_param(line)[1]  # time of measurements
+cps = pars.get_param(line)[2]  # total counts per second
+pars.remove_lines(line, 16)
+# get list of counts per chanel
+counts = pars.get_counts(line, num_of_chanels)
+pars.remove_lines(line, 1030)
+# get list of energy
+energy = pars.get_energy_list(line, num_of_chanels)
+#create dataframe
+df = pd.Series(counts, energy).to_frame().reset_index()
+df.columns = ['Energy', 'Counts']
+print(df)
 
-    #for i in range(len(energy)):
-        #print(energy[i])
-
-    #for i in range(len(line)):
-        #print(line[i])
-
-
-
-
-print(dir(pd))
-#df = pd.Series()
 
