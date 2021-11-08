c46d25c100bc79c9e94bfc8fa1e3647ed0a1e3dd
Aliaksandr Dvornik <aadvornik@yandex.ru>
2021-11-08 15:24:30 +0300

    Finalized

--- data/spec_pars.py
+++ data/spec_pars.py
@@ -1,5 +1,7 @@
 class Parser:
-# TODO: add more functions
+    """
+    This class define all main functions to parse the spectrum file
+    """
     def __init__(self, fileName):
         self.fileName = fileName
         self.fin = open(fileName, 'r')
@@ -21,42 +23,13 @@ class Parser:
     def remove_lines(self, line, n):
         del line[:n]
 
+    def get_counts(self, line, n):
+        counts = [line[i] for i in range(0, n + 1)]
+        return counts
 
-
-
-#read spectrum file
-counts = []
-energy_list=[]
-counts_in_chanel={}
-try:
-    inf = open('spec.spe', 'r')
-    l = [line.strip() for line in inf]
-    del l[:7]
-
-    for i in l:
-        date_mea = l[1]
-        time = int(l[3].split()[0])
-        cps = float(l[5])
-        #print(i)
-    del l[:15]
-    num_of_chanels = int(l[0].split()[1])+1 # reading number of chanels
-    counts = [l[i] for i in range(1, num_of_chanels+2)] # reading counts in each chanel
-    for a in range(1, num_of_chanels):
-        counts_in_chanel[a] = counts[a]
-    del l[:1031]
-    en = [l[i].split() for i in range(0, num_of_chanels)]
-    for elem in en:
-        energy_list.append(elem[1])
-    # for key, value in counts_in_chanel.items():
-    #     print(key, value)
-
-    #for i in range(len(energy_list)):
-        #print(energy_list[i], end ='\n')
-
-    # print(time)
-    # print(date_mea)
-    # print(cps)
-    # print(num_of_chanels)
-
-except IOError:
-    print("No file")
+    def get_energy_list(self, line, n):
+        energy_list = []
+        en = [line[i].split() for i in range(0, n+1)]
+        for elem in en:
+            energy_list.append(elem[1])
+        return energy_list
