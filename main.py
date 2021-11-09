ddb07f02abe1262ab069318dee5ba4c7209dfa99
Aliaksandr Dvornik <aadvornik@gmail.com>
2021-11-10 01:27:45 +0300

    Updated

--- main.py
+++ main.py
@@ -34,8 +34,11 @@ df.get_dataframe(energy,counts,time)
 
 # create GUI
 root = tk.Tk()
+root.title('SpecReader 0.9.1')
 
 canvas = tk.Canvas(root, width= 800, height=600)
-canvas.grid(columnspan=3)
+canvas.grid(columnspan = 3, rowspan = 3)
 
+description = tk.Label(root, text='Browse spectrum file', font = 'Raleway')
+description.grid(column = 0, row=0)
 root.mainloop()
