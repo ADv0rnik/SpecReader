4261fc7b4099009f937dfe3db27b350ba9a10265
Aliaksandr Dvornik <aadvornik@gmail.com>
2021-11-10 00:38:28 +0300

    Updated

--- data/df_constructor.py
+++ data/df_constructor.py
@@ -5,6 +5,8 @@ class DfConstructor:
     def __init__(self):
         pass
 
-    def get_dataframe(self, list1, list2):
+    def get_dataframe(self, list1, list2, time):
         df = pd.Series(list1, list2).to_frame().reset_index()
         df.columns = ['Energy', 'Counts']
+        df['CPS'] = df['Counts'] / int(time)
+        return df
