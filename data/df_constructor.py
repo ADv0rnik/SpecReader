842808b817e8d161286512701cabd60ad4134cb3
Aliaksandr Dvornik <aadvornik@yandex.ru>
2021-11-09 13:09:29 +0300

    Added class

--- data/df_constructor.py
+++ data/df_constructor.py
@@ -1,7 +1,18 @@
 import pandas as pd
 from data.spec_pars import Parser
 
-num_of_chanels = 1023
+
+class DfConstructor:
+
+    def __init__(self):
+        pass
+
+    def get_dataframe(self, list1, list2):
+        df = pd.Series(list1, list2).to_frame().reset_index()
+        df.columns = ['Energy', 'Counts']
+
+
+num_of_channels = 1023
 
 fname = 'spec.spe'
 pars = Parser(fname)
@@ -13,14 +24,8 @@ date_mea = pars.get_param(line)[0]  # date of measurements
 time = pars.get_param(line)[1]  # time of measurements
 cps = pars.get_param(line)[2]  # total counts per second
 pars.remove_lines(line, 16)
-# get list of counts per chanel
-counts = pars.get_counts(line, num_of_chanels)
+# get list of counts per channel
+counts = pars.get_counts(line, num_of_channels)
 pars.remove_lines(line, 1030)
 # get list of energy
-energy = pars.get_energy_list(line, num_of_chanels)
-#create dataframe
-df = pd.Series(counts, energy).to_frame().reset_index()
-df.columns = ['Energy', 'Counts']
-print(df)
-
-
+energy = pars.get_energy_list(line, num_of_channels)
