50e2a3ed7ca71a804f669ac461afb2b231da9094
Aliaksandr Dvornik <aadvornik@yandex.ru>
2021-11-09 15:47:30 +0300

    tkinter problem

--- main.py
+++ main.py
@@ -2,4 +2,32 @@
 version 0.9.1
 by Aliaksandr Dvornik
 """
+import tkinter as tk
+from data.spec_pars import Parser
+from data.df_constructor import DfConstructor
+
+num_of_channels = 1023
+
+fname = 'spec.spe'
+pars = Parser(fname)
+
+# read spectrum file
+line = pars.get_lines()
+pars.remove_lines(line, 7)
+
+# get data from spectrum file
+date_mea = pars.get_param(line)[0]  # date of measurements
+time = pars.get_param(line)[1]  # time of measurements
+cps = pars.get_param(line)[2]  # total counts per second
+pars.remove_lines(line, 16)
+
+# get list of counts per channel
+counts = pars.get_counts(line, num_of_channels)
+pars.remove_lines(line, 1030)
+
+# get list of energy
+energy = pars.get_energy_list(line, num_of_channels)
+
+# create data frame
+root = tk.Tk()
 
