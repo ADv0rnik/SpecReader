9a00260230c9a558bc4eb54507245a2b567e1535
Aliaksandr Dvornik <aadvornik@gmail.com>
2022-01-17 01:56:51 +0300

    Update of class body

--- data/spec_pars.py
+++ data/spec_pars.py
@@ -1,22 +1,31 @@
+import re
+
+
 class Parser:
     """
     This class define all main functions to parse the spectrum file
     """
+
     def __init__(self, file_name):
         self.fileName = file_name
 
     def get_lines(self):
         with open(self.fileName, "r") as f:
             line = [l.strip() for l in f]
-            return line
+        return line
 
-    @staticmethod
-    def get_param(line):
+    def get_cps(self, line):
+        for a, j in enumerate(line):
+            if j == '$CPS:':
+                return float(line[a + 1])
+
+    def get_param(self, line):
         date_mea = ' '
         time = 0
         cps = 0.0
         for _ in line:
-            date_mea = line[1]
+            date_mea = re.match(r'\d{2}/\d{2}/\d{4}', line)
+            # date_mea = line[1]
             time = int(line[3].split()[0])
             cps = float(line[5])
         return date_mea, time, cps
@@ -30,7 +39,20 @@ class Parser:
 
     def get_energy_list(self, line, n):
         energy_list = []
-        en = [line[i].split() for i in range(0, n+1)]
+        en = [line[i].split() for i in range(0, n + 1)]
         for elem in en:
             energy_list.append(int(elem[1]))
         return energy_list
+
+
+num_of_channels = 1023
+
+fname = "C:\\Users\\aadvo\\PycharmProjects\\SpecReader\\spec.spe"
+
+# match = re.search(r'\d{2}/\d{2}/\d{4}', line)
+# if match:
+#     print(match[0])
+pars = Parser(fname)
+cps = pars.get_cps(pars.get_lines())
+
+
