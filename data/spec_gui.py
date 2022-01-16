874ac4698998cd7b5cc56d4aa65d2a73fc1363f2
Aliaksandr Dvornik <aadvornik@gmail.com>
2022-01-17 01:57:25 +0300

    Started to build an interface

--- data/spec_gui.py
+++ data/spec_gui.py
@@ -5,3 +5,12 @@ class BaseInterface:
     def __init__(self):
         pass
 
+root = tk.Tk()
+root.title('SpecReader 0.9.1')
+
+canvas = tk.Canvas(root, width= 800, height=600)
+canvas.grid(columnspan = 3, rowspan = 3)
+
+description = tk.Label(root, text='Browse spectrum file', font = 'Raleway')
+description.grid(column = 0, row=0)
+root.mainloop()
