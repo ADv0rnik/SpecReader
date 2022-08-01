dcb37c0a3b770a292fb05146b65d8f8454194e1a
Alex Dvornik <aadvornik@gmail.com>
2022-08-01 23:39:10 +0300

    Added setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,24 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as readme:
+    long_description = readme.read()
 
 setup(name='spec-reader',
-      version='0.1',
+      version='0.3',
       description='The spectrum reader',
+      long_description=long_description,
       url='https://github.com/ADv0rnik/SpecReader.git',
       author='Aliaksandr Dvornik',
       author_email='aadvornik@gmail.com',
       license='MIT',
-      packages=['reader'],
+      keywords="reader spectrum",
+      packages=find_packages(exclude=['tests*']),
+      include_package_data=True,
+      install_requires=['matplotlib', 'pandas', 'art'],
+      package_data={
+          "sample": ['sample.spe']
+      },
+      entry_points={
+            "console_scripts": ['sreader=main.py']
+      },
       zip_safe=False)
