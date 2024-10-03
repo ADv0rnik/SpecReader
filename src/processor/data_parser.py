029c28a803c95bb66c8e31bf5da32da115f41570
Alex Dvornik <aadvornik@gmail.com>
2024-10-03 12:01:40 +0200

    Updated tests

--- src/processor/data_parser.py
+++ src/processor/data_parser.py
@@ -6,7 +6,7 @@ from typing import List
 class DataParser:
 
     @staticmethod
-    def __split_line(line: str) -> List[str]:
+    def split_line(line: str) -> List[str]:
         return line.strip().split()
 
     def read_from_file(self, file, queue_: Queue):
@@ -17,7 +17,7 @@ class DataParser:
             lines = [line.strip() for line in f]
             for i, line in enumerate(lines):
                 if line == "$MEAS_TIM:":
-                    live_time = int(self.__split_line(lines[i + 1])[0])
+                    live_time = int(self.split_line(lines[i + 1])[0])
                 if line == "$DATA:":
                     start_index = int(i + 2)
                     break
--- tests/test_reader.py
+++ tests/test_reader.py
@@ -1,11 +1,10 @@
 import unittest
-
-import pandas as pd
 import pytest
+from numpy.ma.testutils import assert_equal
 
 from src.processor.data_parser import DataParser
-from src.main import save_to_dataframe, parse_arguments
-from argparse import Namespace
+from src.main import save_to_dataframe
+
 
 
 @pytest.fixture(scope="session")
@@ -15,12 +14,20 @@ def mk_tmp_dir(tmp_path_factory):
 
 
 def test_save_to_dataframe(mk_tmp_dir):
-    test_data =  {'test_data.Spe': [0, 0, 0, 0, 0, 14, 907, 2486, 2363, 1972, 1810, 1691, 1767, 2021, 0, 378]}
-    result = save_to_dataframe(mk_tmp_dir, test_data)
-    assert type(result) == pd.DataFrame
+    test_dict =  {'test_data.Spe': [0, 0, 0, 0, 0, 14, 907, 2486, 2363, 1972, 1810, 1691, 1767, 2021, 0, 378]}
+    test_none = None
+    save_to_dataframe(mk_tmp_dir, test_dict)
+    save_to_dataframe(mk_tmp_dir, test_none)
+
 
 
 class ReaderTests(unittest.TestCase):
 
     def setUp(self):
         self.data_parser = DataParser()
+        self.test_line = "144 146 "
+
+    def test_split_line(self):
+        result = self.data_parser.split_line(self.test_line)
+        assert_equal(result, ['144', '146'])
+        assert_equal(isinstance(result, list), True)
